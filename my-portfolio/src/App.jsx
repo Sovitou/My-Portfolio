9a7a4de13954f0af635853a99aa466aa569e51b7
Vitou23 <sk6024010077@camtech.edu.kh>
2024-11-12 22:57:46 +0700

    Enhance App component layout with background styling and structure

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -10,7 +10,13 @@ import Contact from "./Components/contact";
 
 export default function App() {
   return (
-    <div>
+    <div className="relative min-h-full">
+      {/* Background */}
+      <div className="absolute inset-0 -z-10 min-w-full min-h-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+        <div className="absolute inset-x-0 top-0 -z-10 m-auto size-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]" />
+      </div>
+      
+      {/* Components */}
       <Navbar />
       <Hero />
       <Aboutme />
